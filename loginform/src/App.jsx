1823d3931d12ee9e347928b32d889abf540fb3a8
Femi Adedokun <adedokunfemi14@gmail.com>
2023-10-22 04:33:34 -0700

    done with the frontend

--- loginform/src/App.jsx
+++ loginform/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from './Signup'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './Login'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Signup />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
